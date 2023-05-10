31ef7b90101da33ff980e4271455c6539a9f32c1
Joseph Alikah <alikahjoseph@gmail.com>
2023-05-10 02:51:49 +0100

    0x01 - Add task 101

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Top students."""
+
+
+def top_students(mongo_collection):
+    """Return a list of the top students."""
+    students = mongo_collection.aggregate([
+        {
+            "$addFields": {
+                "averageScore": {"$avg": "$topics.score"}
+            }
+        },
+    ])
+    return list(students)
